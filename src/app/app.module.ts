50e2b1ea10ac68c59680fa237e6568f30be33bac
Juan Cruz <juancruzf017@gmail.com>
2023-08-04 19:06:40 -0300

    Add Module y Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+//Angular Material
+import { MaterialModule } from './material/material.module'
+
+
+//Routing
+import { AppRoutingModule } from './app-routing/app-routing.module'
+
+//Componentes
 import { AppComponent } from "./app.component";
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
